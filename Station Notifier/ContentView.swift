bd18a67cdaaab3c218f7a3a833296e0e1be3cfc1
Ryan Stone <rstone4@chewy.com>
2019-06-07 07:27:23 -0400

    Initial Commit

---
+++ Station Notifier/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Station Notifier
+//
+//  Created by Ryan Stone on 6/7/19.
+//  Copyright © 2019 Ryan Stone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
