46fe5d764ca17dfc1e6c3314c66198a95564d696
Ryan Stone <ryan@ryanstone.io>
2019-09-11 21:16:03 -0400

    Location State conforms to Codable

--- Station Notifier/Flux/State/LocationState.swift
+++ Station Notifier/Flux/State/LocationState.swift
@@ -2,38 +2,28 @@ import Foundation
 import Combine
 import CoreLocation
 
-class LocationState {
+struct LocationState: Equatable {
     
     private (set) var location: CLLocation = CLLocation()
-    private (set) var latitude: CLLocationDegrees   = CLLocationDegrees()
-    private (set) var longitude: CLLocationDegrees  = CLLocationDegrees()
-    private (set) var timeStamp: Date               = Date()
-    private (set) var speed: CLLocationSpeed        = CLLocationSpeed()
-    private (set) var course: CLLocationDirection   = CLLocationDirection()
+    
+    var longitude: CLLocationDegrees { location.coordinate.longitude }
+    var latitude: CLLocationDegrees { location.coordinate.latitude }
+    var timeStamp: Date { location.timestamp }
+    var speed: CLLocationSpeed { location.speed }
+    var course: CLLocationDirection { location.course }
 
-    func add(location: CLLocation) {
+    mutating func add(location: CLLocation) {
         self.location   = location
-        self.latitude   = location.coordinate.latitude
-        self.longitude  = location.coordinate.longitude
-        self.timeStamp  = location.timestamp
-        self.speed      = location.speed
-        self.course     = location.course
     }
 }
 
 extension LocationState: Decodable {
     private enum CodingKeys: String, CodingKey {
-        case
-        location,
-        latitude,
-        longitude,
-        timeStamp,
-        speed,
-        course
+        case location
     }
 
-    required init(from decoder: Decoder) throws {
-        var container = try decoder.container(keyedBy: CodingKeys.self)
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
 
         guard let location = { try? container.decode(Data.self, forKey: .location) }()
             .flatMap({ data in try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? CLLocation }) else {
@@ -41,27 +31,23 @@ extension LocationState: Decodable {
             throw DecodingError.typeMismatch(Data.self, DecodingError.Context(codingPath: [CodingKeys.location], debugDescription: "Failed to save location"))
         }
 
-        guard let speed = { try? container.decode(Data.self, forKey: .speed) }()
-           .flatMap({ data in try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? CLLocationSpeed }) else {
-
-           throw DecodingError.typeMismatch(Data.self, DecodingError.Context(codingPath: [CodingKeys.speed], debugDescription: "Failed to save speed"))
-       }
-
-        guard let course = { try? container.decode(Data.self, forKey: .course) }()
-              .flatMap({ data in try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? CLLocationDirection }) else {
-
-              throw DecodingError.typeMismatch(Data.self, DecodingError.Context(codingPath: [CodingKeys.course], debugDescription: "Failed to save course"))
-          }
-
         self.location = location
-        self.course = course
-        self.speed = speed
-        self.timeStamp = try container.decode(Date.self, forKey: .timeStamp)
-        self.latitude = try container.decode(Double.self, forKey: .latitude)
-        self.longitude = try container.decode(Double.self, forKey: .longitude)
     }
 }
 
 extension LocationState: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        let locationData = try NSKeyedArchiver.archivedData(withRootObject: location, requiringSecureCoding: false)
+        
+        try container.encode(locationData, forKey: .location)
+    }
+}
 
+extension CLLocationCoordinate2D: Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        return lhs.latitude == rhs.latitude &&
+            lhs.longitude == rhs.longitude
+    }
 }
